45a9044e6a92e82af976c71b0367556fd50ec237
Rolando Pereira <finalyugi@sapo.pt>
2010-06-14 23:43:27 +0100

    Created the Slashorg command.

--- slashorg.el
+++ slashorg.el
@@ -28,16 +28,18 @@ it should return News: Lorem Ipsum"
     (t
       (concat "" (get-info (cdr list))))))
 
-(defun teste2 ()
+(defun slashorg-insert-content-into-buffer ()
   (interactive)
   (let ((max-lisp-eval-depth 2000000) 
          (max-specpdl-size 20000))
     (insert (get-info (get-frontpage-as-list)))
     (fill-region (point-min) (point-max))))
 
-(car (org-split-string
-  (cadr (org-split-string "Comments: 60 + -   Technology: BlackBerry Predicted a Century Ago By Nikola Tesla on Tuesday May 04, @08:53AM" "+ -[[:space:]]*"))
-  ;; This regexp removes the end of the headline (the part with the day of the
-  ;; week, name of the month, day of the month (in numerical format), and the
-  ;; hour the news was posted
-  " on [[:ascii:]]+ [[:ascii:]]+ [0-3][0-9], @[[:digit:]]*:[[:digit:]]*[AP]M$"))
+(defun slashorg ()
+  (interactive)
+  (switch-to-buffer "*Slashorg*")
+  (org-mode)
+  (erase-buffer)
+  (slashorg-insert-content-into-buffer)
+  (org-cycle-internal-global) ;; Change the visibility to OVERVIEW
+  (goto-char (point-min)))
